78eba7065ef83746820132588445645ca8ea46fa
Quentin <quentin.garraud@viacesi.fr>
2020-12-18 09:54:19 +0100

    feat : interdire les caractères spéciaux et les chiffres, recevoir des messages

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 //  Definir le pseudo  //
 let User = {
     set_pseudo: function(value){
+        var champ = document.getElementById('input--pseudo');
+        
         document.getElementById('display-pseudo').innerHTML = "Bonjour " + value + " !"
+        champ.style.backgroundColor = "transparent";
     }
 }
 
@@ -59,10 +62,14 @@ let Properties = {
 
     },
     change_size: function(size){
-        var message = document.getElementsByClassName('item-message');
+        var myMessage = document.getElementsByClassName('item-message');
+        var otherMessage = document.getElementsByClassName('first-message');
 
-        for (var i = 0; i < message.length; i++) {
-            message[i].style.fontSize = size + "px";
+        for (var i = 0; i < otherMessage.length; i++) {
+            otherMessage[i].style.fontSize = size;
+        }
+        for (var i = 0; i < myMessage.length; i++) {
+            myMessage[i].style.fontSize = size;
         }
     }
 }
@@ -70,7 +77,7 @@ document.getElementById('button--change-color').addEventListener('click',functio
     Properties.change_color(document.getElementById('input--change-color').value)
 })
 document.getElementById('button--change-size').addEventListener('click',function(){
-    Properties.change_size(document.getElementById('input--change-size').value)
+    Properties.change_size(document.getElementById('select--change-size').value)
 })
 
 let Helpers = {
@@ -104,3 +111,54 @@ function displayDate(){
 }
 displayDate();
 setInterval(displayDate, 1000);
+
+
+// Pouvoir recevoir des messages
+var url = "https://todo.ezze-indev.com/"
+var xhttp = new XMLHttpRequest();
+
+
+// recuperer contenu
+xhttp.onreadystatechange = function() {
+      // lire contenu
+    if (this.readyState == 4 && this.status == 200) {
+        //console.log(xhttp.responseText);
+        var data = JSON.parse(xhttp.responseText);
+
+        var content = "";
+
+        // Afficher l'heure du message
+        var chatGetDate = new Date();
+        var chatGetHour = chatGetDate.getHours();
+        var chatGetMinutes = chatGetDate.getMinutes();
+
+
+        // boucle
+        for (const key of Object.keys(data)) {
+            content += "<li class='first-message'>" + "<i class='user-message'> Par "+ data[key].user +"</i>" + data[key].name + "<p class='message-hour'>" + "Reçu à " + chatGetHour + ":" + chatGetMinutes + "</p>" + "</li>"
+        }
+
+        document.getElementById("step-message-list").innerHTML = content;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+
+
+  function verifierCaracteres(event) {
+	 		
+	var keyCode = event.which ? event.which : event.keyCode;
+	var touche = String.fromCharCode(keyCode);
+			
+	var champ = document.getElementById('input--pseudo');
+			
+	var caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			
+	if(caracteres.indexOf(touche) >= 0) {
+        champ.value += touche;
+        champ.style.backgroundColor = "transparent";
+	} else {
+        champ.style.backgroundColor = "red";
+    }
+			
+}
